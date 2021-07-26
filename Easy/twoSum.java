add5a3a24ef240beebc1943f57976a90096454df
pairofAces <karanchauhan16@gmail.com>
2021-07-25 23:40:25 -0400

    created initial for loop, working on nested for loop structure

--- Easy/twoSum.java
+++ Easy/twoSum.java
@@ -2,8 +2,12 @@
 
 import java.util;
 
+// Solution 1, brute force
 class Program{
     public static int[] twoNumberSum(int[] array, int targetSum) {
-        
+        // initiate the first for loop to traverse the array
+        for (int i = 0; i < array.length - 1; i++) {
+            
+        }
     }
 }
