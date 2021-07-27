99002f31b598d017feafb56de760a8d16cfc2d1d
pairofAces <karanchauhan16@gmail.com>
2021-07-26 22:39:35 -0400

    working on nested for loop, class Program

--- Easy/twoSum.java
+++ Easy/twoSum.java
@@ -7,7 +7,13 @@ class Program{
     public static int[] twoNumberSum(int[] array, int targetSum) {
         // initiate the first for loop to traverse the array
         for (int i = 0; i < array.length - 1; i++) {
+            // create a pointer to the first number
+            int firstNum = array[i];
             
+            // initiate nested for loop with a second pointer
+            for(int j = i + 1; j < array.length; j++) {
+                
+            }
         }
     }
 }
