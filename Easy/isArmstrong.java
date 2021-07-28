b02383f4b81e04b440d02618a805eef3e2f05d22
pairofAces <karanchauhan16@gmail.com>
2021-07-27 21:16:40 -0400

    creating a program to check if a number input is an armstrong

---
+++ Easy/isArmstrong.java
@@ -0,0 +1,11 @@
+// create a program that will check to see if a number
+// is an armstrong
+
+// an armstrong is an n-digit number that is equal to
+// the sum of the nth powers of its individual digits
+
+class Program {
+    public static void main(String[] args) {
+        // initiate starting variables
+    }
+}
