17bd712a36ae98da292acd1905e915270f9e3b66
pairofAces <karanchauhan16@gmail.com>
2021-07-28 23:05:45 -0400

    creating logic for the nested for loop

--- Easy/reverseWordsInString.java
+++ Easy/reverseWordsInString.java
@@ -5,6 +5,22 @@ class Program {
 
         // create a variable to represent the final reversed string
         String reversed = "";
-        
+
+        // iterate over the words array
+        for (int i = 0; i < words.length; i++) {
+            // create variable to represent each word during the
+            // traversal
+            String word = words[i];
+            // create second empty string variable to represent
+            // the singular word that will be reversed
+            String reversedWord = "";
+
+            // initate nested for loop to iterate over the single word
+            // and add it into the reversedWord variable
+            for (int j = word.length() - 1; j >= 0; j--) {
+                
+            }
+
+        }
     }
 }
