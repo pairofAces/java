60742e7a38198439918b2b3511c482b5a3481280
pairofAces <karanchauhan16@gmail.com>
2021-07-30 22:39:56 -0400

    creating optimized version for twoNumberSum in java

--- Easy/twoSum.java
+++ Easy/twoSum.java
@@ -3,6 +3,9 @@
 import java.util;
 
 // Solution 1, brute force
+    // Time: O(n^2) time, where (n) is the length of the array
+
+    // Space: O(1), constant time, since no external data structures are being used
 class Program{
     public static int[] twoNumberSum(int[] array, int targetSum) {
         // initiate the first for loop to traverse the array
@@ -21,5 +24,14 @@ class Program{
         // if there were no integers that summed to the targetSum
         // return 0
         return new int[0];
+    }
+}
+
+// Solution 2 - optimized
+// Still using the import java.util.*; statement from above
+
+class Program{
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        
     }
 }
