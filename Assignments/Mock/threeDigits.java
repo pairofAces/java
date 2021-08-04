a84443f638fa8ac28cbd8684c1e7af51b9abba70
pairofAces <karanchauhan16@gmail.com>
2021-08-04 11:54:26 -0400

    reference to 3 digits question

---
+++ Assignments/Mock/threeDigits.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class threeDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeSet<String> treee = new TreeSet<String>();
+        List<String> output = new LinkedList<String>();
+        List<Integer> inarr = new ArrayList<Integer>();
+        
+        String inputLine = sc.nextLine();
+        String[] str = inputLine.split(",");
+        for(String elem: str) {
+            inarr.add(Integer.parseInt(elem));
+        }
+        sc.close();
+        
+        for(int i = 0; i < inarr.size(); i++) {
+            for(int j = 0; j < inarr.size(); j++) {
+                for(int k = 0; k < inarr.size(); k++) {
+                    if(i==j || i==k ||j==k) {
+                        continue;
+                    }
+                    String s = Integer.toString(inarr.get(i))+Integer.toString(inarr.get(j))+Integer.toString(inarr.get(k));
+                    treee.add(s);
+                }
+            }
+        }
+        System.out.println(treee);
+        int numCombinations = treee.size();
+        String max = treee.last();
+        System.out.println(max + "," + numCombinations);
+    }
+    
+}
