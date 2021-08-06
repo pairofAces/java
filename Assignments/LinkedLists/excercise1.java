e33450e54a2653fdcbafc7e3bcb8017508b3a17f
pairofAces <karanchauhan16@gmail.com>
2021-08-06 12:24:25 -0400

    generating code to concatenate list2 to list1, in reverse order

---
+++ Assignments/LinkedLists/excercise1.java
@@ -0,0 +1,21 @@
+// Given two lists, concatenate the second list in reverse order to the first
+// list and return the concatenated list. Implement the logic inside the
+// concatenateLists() method.
+
+// Note - use the descendingIterator() method to iterate over the elements
+// in reverse order
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+class Tester {
+    public static List<Object> concatenateLists(List<Object> listOne, List<Object>listTwo) {
+        // code implementation here
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
