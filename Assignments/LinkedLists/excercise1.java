b9adb922c7c5370529e2001c005c8fe6c2e25646
pairofAces <karanchauhan16@gmail.com>
2021-08-06 12:55:30 -0400

    created public static void main, implementing code to concatenate the lists

--- Assignments/LinkedLists/excercise1.java
+++ Assignments/LinkedLists/excercise1.java
@@ -16,6 +16,23 @@ class Tester {
     }
 
     public static void main(String[] args) {
-        
+        List<Object> listOne = new LinkedList<Object>();
+        listOne.add("Hello");
+        listOne.add(102);
+        listOne.add(25);
+        listOne.add(38.5);
+
+        List<Object> listTwo = new LinkedList<Object>();
+        listTwo.add(150);
+        listTwo.add(200);
+        listTwo.add('A');
+        listTwo.add("Welcome");
+
+        List<Object> concatenatedList = concatenateLists(listOne, listTwo);
+
+        System.out.println("Concatenated linked list:");
+        for (Object value : concatenatedList) {
+            System.out.print(value + " ");
+        }
     }
 }
