a4bcbf0ae56e85205f6aa1534ecab09ff5943975
pairofAces <karanchauhan16@gmail.com>
2021-08-07 22:49:56 -0400

    created logic to check the elements and increment the sequence pointer

--- Easy/validateSubsequence.java
+++ Easy/validateSubsequence.java
@@ -8,6 +8,12 @@ class Solution1 {
         // the array and the sequence
         int arrayPointer = 0;
         int seqPointer = 0;
+        // while the pointers are less than their repective arrays
+        while (arrayPointer < array.size() && seqPointer < sequence.size()) {
+            // if the the elements at the index positions of each pointer are equal
+            // then increment the sequence pointer
+            seqPointer++;
+        }
     }
 }
 
