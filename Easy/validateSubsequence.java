c24fa6f33790621953e5f1ab924d198d940210a0
pairofAces <karanchauhan16@gmail.com>
2021-08-07 22:45:04 -0400

    implementing code to validate subsequence

---
+++ Easy/validateSubsequence.java
@@ -0,0 +1,10 @@
+// Validate Subsequence
+
+import java.util.*;
+
+class Solution1 {
+    public static boolean validateSubsequence(List<Integer> array, List<Integer> sequence) {
+        
+    }
+}
+
