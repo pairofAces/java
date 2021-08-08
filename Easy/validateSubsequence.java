3cc6b3a465dde264a5e82857a32c904f41229cf8
pairofAces <karanchauhan16@gmail.com>
2021-08-07 22:47:42 -0400

    create pointers, generating while loop

--- Easy/validateSubsequence.java
+++ Easy/validateSubsequence.java
@@ -4,7 +4,10 @@ import java.util.*;
 
 class Solution1 {
     public static boolean validateSubsequence(List<Integer> array, List<Integer> sequence) {
-        
+        // create two pointer variables to keep track of the indices in
+        // the array and the sequence
+        int arrayPointer = 0;
+        int seqPointer = 0;
     }
 }
 
