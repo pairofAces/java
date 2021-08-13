5b2f315899f9fde33a4f39c57d8b62c218d815a6
pairofAces <karanchauhan16@gmail.com>
2021-08-12 23:52:23 -0400

    while loop logic complete

--- Easy/palindromeNumber.java
+++ Easy/palindromeNumber.java
@@ -9,7 +9,18 @@ class Solution {
 
         // initiate while loop for as long as the input isn't 0
         while (x != 0) {
-            
+            // the remainder variable will hold the last digit of the input number
+            remainder = x % 10;
+
+            // the reversed variable will multiply itself by 10, then add the value
+            // of the remainder, so that the remainder can be stored at the next
+            // decimal place
+            reversed = reversed * 10 + remainder;
+
+            // after the last digit of the input is added to our new variable, reversed,
+            // remove the last digit of the input number by dividing the input by 10
+            x /= 10;
         }
+        
     }
 }
