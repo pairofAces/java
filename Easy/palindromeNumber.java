ae4e30bba2c9d887e0bc94801b4b32150ba0c0cf
pairofAces <karanchauhan16@gmail.com>
2021-08-12 23:49:31 -0400

    creating while loop logic

--- Easy/palindromeNumber.java
+++ Easy/palindromeNumber.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
+        // handle edge case, if the input number is less than 0
+        // return false
+        if (x < 0) return false;
+
+        // create comparison variables
+        int reversed = 0, remainder, original = num;
+
+        // initiate while loop for as long as the input isn't 0
+        while (x != 0) {
+            
+        }
     }
 }
