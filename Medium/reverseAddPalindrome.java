a58a2fd658b9ecdebb87a172e0d79591e3f99764
pairofAces <karanchauhan16@gmail.com>
2021-08-13 22:56:32 -0400

    created template for reverseAddPalindrome

---
+++ Medium/reverseAddPalindrome.java
@@ -0,0 +1,15 @@
+// How to reverse and add a number until you get a palindrome
+    // take the input number
+        // if palindrome -> true
+        // if not, add the reverse of the input, to the input
+
+import java.util.Scanner;
+
+class ReverseAddPalindrome {
+    // create methods to handle the following:
+        // (1) reverse the number
+        // (2) check if the number is a palindrome
+        // (3) then, reverse and add the number until the number is a palindrome
+
+    // method to reverse the number
+}
