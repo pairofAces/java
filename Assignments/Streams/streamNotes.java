488cb6d1a5556d20b38a5fd6f92879ab24b1d3a7
pairofAces <karanchauhan16@gmail.com>
2021-08-19 10:49:14 -0400

    example for creating a stream of objects

--- Assignments/Streams/streamNotes.java
+++ Assignments/Streams/streamNotes.java
@@ -21,3 +21,22 @@ Stream<String> supernatural = castList.stream();
 // creating a stream from an array
 Integer[] array = {672, 340, 999};
 Stream<Integer> streamFromArray = Arrays.stream(array);
+
+// ----------------------------------------------------------------------
+
+// Creating a stream of objects
+
+// user defined class Employee
+
+class Employee {
+    private String name;
+    private Double salary;
+    
+    public Employee(String name, Double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
+
+// creating the stream
+Stream<Employee> empStream = Stream.of(new Employee("Tom", 5699.5), new Employee("Jack", 7629.2), new Employee("Jane", 5289.8));
