cf4505afd15f167afde4619b558c30ebeed0ce3f
pairofAces <karanchauhan16@gmail.com>
2021-08-18 23:36:52 -0400

    creating notes for streams

---
+++ Assignments/Streams/streamNotes.java
@@ -0,0 +1,12 @@
+// creating and adding elements into a list-arrayList
+
+// ArrayList of strings
+List<String> stringList = new ArrayList<String>();
+stringList.add("Abigail");
+stringList.add("Zachariah");
+stringList.add("Damien");
+stringList.add("Ralph");
+stringList.add("Gabriel");
+
+// sort the list with the inbuilt (sort) method, using lambda function
+stringList.sort()
---
+++ Easy/romanToInt.java
@@ -0,0 +1,2 @@
+// Roman numeral to integer
+
