4f25e3ff16396ecf1d87b042727b9f5a3717dfc1
pairofAces <karanchauhan16@gmail.com>
2021-08-20 19:26:06 -0400

    generating for loop logic

--- Easy/romanToInt.java
+++ Easy/romanToInt.java
@@ -3,6 +3,11 @@
 class Solution {
     public int romanToInt(String s) {
         // create refernce array
-        
+        int nums[] = new int[s.length()];
+
+        // initiate the for loop
+        for (int i = 0; i < s.length(); i++) {
+            
+        }
     }
 }
