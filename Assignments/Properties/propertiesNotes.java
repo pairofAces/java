85331f990dee2b0f9698c6470edee89f6f383719
pairofAces <karanchauhan16@gmail.com>
2021-08-24 22:13:23 -0400

    generated example code for properties -> configurations and methods

--- Assignments/Properties/propertiesNotes.java
+++ Assignments/Properties/propertiesNotes.java
@@ -6,3 +6,29 @@
 
 org.apache.commons.configuration.PropertiesConfiguration
     // this loads values from the properties file
+
+// To configure the PropertiesConfiguration class to load the properties file:
+
+Configurations configurations = new Configurations();
+PropertiesConfiguration config = configurations.properties("configuration.properties");
+    
+    // On lines 12-13, an object of the PropertiesConfiguration class is built.
+    // this object can be used to access data form the specified properties file.
+
+// Note, if I want to pass the properties name as shown on lines 12-13, keep the
+// properties file in the src folder
+
+
+// METHODS:
+
+    // the PropertiesConfiguration class has a few methods like:
+
+Iterator<String> getKeys() : // this will provide all the keys present in the properties file
+    // example:
+    Iterator<String> keys = config.getKeys();
+
+Object getProperty(String key): // This provides the corresponding value present in the properties
+                                // that belongs to the key passes. If the key value isn't there,
+                                // it will return null.
+    // example:
+    System.out.println("Value for the key thisIsTheKey = " + config.getProperty("thisIsTheKey"));
