c19ba0bd7e4b5e5f6d265f5f1fdeae9b0f0f958a
pairofAces <karanchauhan16@gmail.com>
2021-08-25 23:20:48 -0400

    generating function to invert binary  tree

---
+++ Medium/invertBinaryTree.java
@@ -0,0 +1,22 @@
+// Invert Binary Search Tree
+
+// write a function that takes in a binary tree and inverts it
+    // the function should swap every left node for its corresponding right node
+
+import java.util.*;
+
+class Program {
+    public static void invertBinaryTree(BinaryTree tree) {
+
+    }
+
+    static class BinaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+}
