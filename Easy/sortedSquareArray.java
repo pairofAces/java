2a247cd6b691ec3721f8cf059b20fc7698babcd5
pairofAces <karanchauhan16@gmail.com>
2021-09-01 22:57:44 -0400

    implementing for loop logic

--- Easy/sortedSquareArray.java
+++ Easy/sortedSquareArray.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 class Program {
     public int [] sortedSquareArray(int [] array) {
-        
+        // create an array of integers to be the same size of the input array
+        int [] sortedSquares = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            
+        }
     }
 }
