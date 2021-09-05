31fd3a289ef859ea7868ace2fedf74952cab35e5
pairofAces <karanchauhan16@gmail.com>
2021-09-05 14:48:12 -0400

    created for loop logic

--- Easy/sortedSquareArray.java
+++ Easy/sortedSquareArray.java
@@ -7,7 +7,13 @@ class Program {
         // create an array of integers to be the same size of the input array
         int [] sortedSquares = new int[array.length];
         for (int i = 0; i < array.length; i++) {
-            
+            // create a reference to the element at the (i)th index
+            int value = array[i];
+
+            // insert the square of the (value) into the (sortedSquares) array 
+            // initially created
+            sortedSquareArray[i] = value * value;
         }
+        
     }
 }
