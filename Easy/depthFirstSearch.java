4b8559a01c7c175b905c12c4ae9abc036149e5d1
pairofAces <karanchauhan16@gmail.com>
2021-09-06 22:54:26 -0400

    working on depthFirstSearch class method

--- Easy/depthFirstSearch.java
+++ Easy/depthFirstSearch.java
@@ -6,6 +6,22 @@ import java.util.*;
 
 class Program {
     static class Node {
-        
+        // initiate a variable to hold a string value
+        String name;
+
+        // create a list that will contain nodes
+        List<Node> children = new ArrayList<Node>();
+
+        public Node (String name) {
+            this.name = name;
+        }
+
+        // create a class method that will search through an input array
+        public List<String> depthFirstSearch(List<String> array) {
+            // add the (name) into the input array
+            array.add(this.name);
+
+            
+        }
     }
 }
