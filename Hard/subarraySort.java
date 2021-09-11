368a477f88f6b27cfeb99ac3deb7df4191d7a34e
pairofAces <karanchauhan16@gmail.com>
2021-09-10 22:20:12 -0400

    generating logic within for loop

--- Hard/subarraySort.java
+++ Hard/subarraySort.java
@@ -4,6 +4,13 @@ import java.util.*;
 
 class Program {
     public static int[] subarraySort(int[] array) {
-        
+        // create two variables to represent the max and min
+        int minOutOfOrder = Integer.Max_VALUE;
+        int maxOutOfOrder = Integer.MIN_VALUE;
+
+        // initiate for loop to traverse the input array
+        for(int i = 0; i < array.length; i++) {
+            
+        }
     }
 }
