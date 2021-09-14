308345de1ec73af43fc3edf9a3c5d9a18521f85b
pairofAces <karanchauhan16@gmail.com>
2021-09-13 22:33:42 -0400

    created logic scope and finalized solution for class Program, creating the time/space complexity analysis

--- Hard/subarraySort.java
+++ Hard/subarraySort.java
@@ -20,32 +20,56 @@ class Program {
                 minOutOfOrder = Math.min(minOutOfOrder, num);
                 maxOutOfOrder = Math.max(maxOutOfOrder, num);
             }
+        }
 
-            if (minOutOfOrder == Integer.MAX_VALUE) {
+        if (minOutOfOrder == Integer.MAX_VALUE) {
                 return new int[] {-1, -1};
             }
 
-            // create an index variable to initiate at 0
-            int subarrayLeftIndex = 0;
+        // create an index variable to initiate at 0
+        int subarrayLeftIndex = 0;
 
-            // create a while loop to check if the element at
-            // the index of (subarrayLeftIndex) is less than or
-            // equal to the value of (minOutOfOrder)
-            while (minOutOfOrder >= array[subarrayLeftIndex]) {
-                // increment the left index variable
-                subarrayLeftIndex++;
-            }
-
-            // create the index variable to start at the ending of the
-            // array
-            int subarrayRightIndex = array.length - 1;
-
-            // create a while loop to check if the element at the index
-            // of (subarrayRightIndex) is greater than or equal to
-            // the value of (maxOutOfOrder)
-            while (maxOutOfOrder >= array[subarrayRightIndex]) {
-                subarrayRightIndex--;
-            }
+        // create a while loop to check if the element at
+        // the index of (subarrayLeftIndex) is less than or
+        // equal to the value of (minOutOfOrder)
+        while (minOutOfOrder >= array[subarrayLeftIndex]) {
+            // increment the left index variable
+            subarrayLeftIndex++;
         }
+
+        // create the index variable to start at the ending of the
+        // array
+        int subarrayRightIndex = array.length - 1;
+
+        // create a while loop to check if the element at the index
+        // of (subarrayRightIndex) is greater than or equal to
+        // the value of (maxOutOfOrder)
+        while (maxOutOfOrder >= array[subarrayRightIndex]) {
+            subarrayRightIndex--;
+        }
+
+        // return a new array of the values of the index variables
+        return new int[] {subarrayLeftIndex, subarrayRightIndex};
+    }
+
+    // create helper method
+    public static boolean isOutOfOrder(int i, int num, int[] array) {
+        // create if logic to check if the (i) input is equal to either
+        // the starting or ending index of the input (array)
+        if (i == 0) {
+            // return the boolean of the result of checking if the 
+            // input (num) is greater than the element
+            // in (array) at the index of (i + 1)
+            return num > array[i  + 1];
+        }
+
+        if (i == array.length - 1) {
+            // return the boolean of the result of checking if the 
+            // input (num) is less than the element
+            // in (array) at the index of (i - 1)
+            return num < array[i - 1];
+        }
+
+        return num > array[i + 1] || num < array[i - 1];
     }
 }
