2d9e9e14ecb29c1a86f7126bae50c6575013f26d
pairofAces <karanchauhan16@gmail.com>
2021-09-19 21:24:01 -0400

    created breadthFirstSearch logic

--- Medium/breadthFirstSearch.java
+++ Medium/breadthFirstSearch.java
@@ -12,10 +12,25 @@ public class breadthFirstSearch {
         public Node(String name) {
             this.name = name;
         }
-    }
-
-    public List<String> breadthFirstSearch(List<String> array) {
-        Queue<Node> queue = new LinkedList<Node>();
-        queue.add(this);
+        
+        public List<String> breadthFirstSearch(List<String> array) {
+            Queue<Node> queue = new LinkedList<Node>();
+            queue.add(this);
+    
+            while (!queue.isEmpty()) {
+                Node current = queue.poll();
+                array.add(current.name);
+                queue.addAll(current.children);
+            }
+    
+            return array;
+        }
+    
+        // class method to add a node child
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
     }
 }
