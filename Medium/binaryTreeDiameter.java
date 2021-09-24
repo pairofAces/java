88e859fabe30b3ceb749fa6daf1600cd88444706
pairofAces <karanchauhan16@gmail.com>
2021-09-23 22:08:37 -0400

    created static class method for binaryTreeDiameter

---
+++ Medium/binaryTreeDiameter.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class binaryTreeDiameter {
+    // create binary tree class
+    static class binaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+}
