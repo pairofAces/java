44b2b1db795ba82b4fcfe3a816aad43e4ac5e6ae
pairofAces <karanchauhan16@gmail.com>
2021-09-24 19:09:23 -0400

    generating helper function

--- Medium/binaryTreeDiameter.java
+++ Medium/binaryTreeDiameter.java
@@ -1,6 +1,13 @@
 import java.util.*;
 
 public class binaryTreeDiameter {
+    public int binTreeDiameter(BinaryTree tree) {
+        // create helper method
+        return getTreeInfo(tree).diameter;
+    }
+
+    // create helper function here
+    
     // create binary tree class
     static class binaryTree {
         public int value;
@@ -10,5 +17,7 @@ public class binaryTreeDiameter {
         public BinaryTree(int value) {
             this.value = value;
         }
+
+
     }
 }
