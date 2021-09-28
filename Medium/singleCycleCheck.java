2e4cee789a463cbb39ee28007e42ae94b3676001
pairofAces <karanchauhan16@gmail.com>
2021-09-27 23:13:41 -0400

    generating solution for singleCycleCheck

---
+++ Medium/singleCycleCheck.java
@@ -0,0 +1,14 @@
+public class singleCycleCheck {
+    public static boolean hasSingleCycle(int[] array) {
+        int numElementsVisited = 0;
+        int currentIdx = 0;
+
+        while (numElementsVisited < array.length) {
+
+        }
+
+        // return statement (since the method is 
+        // supposed to return a boolean value)
+        return currentIdx == 0;
+    }
+}
