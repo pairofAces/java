93adc0927c9a813cfb0d4ecf06b35c4bfb584183
pairofAces <karanchauhan16@gmail.com>
2021-09-28 21:41:51 -0400

    creating helper function

--- Medium/singleCycleCheck.java
+++ Medium/singleCycleCheck.java
@@ -4,11 +4,21 @@ public class singleCycleCheck {
         int currentIdx = 0;
 
         while (numElementsVisited < array.length) {
-
+            if(numElementsVisited > 0 && currentIdx == 0) {
+                return false;
+            }
+            numElementsVisited++;
+            
+            //invoke helper function and reference the return value
+            //set to the (currentIdx) 
+            currentIdx = getNextIdx(currentIdx, array);
         }
 
         // return statement (since the method is 
         // supposed to return a boolean value)
         return currentIdx == 0;
     }
+
+    // create helper function
+    
 }
