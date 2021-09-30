4f6a8bb9398376ba79c59db676a9168856e5cff0
pairofAces <karanchauhan16@gmail.com>
2021-09-29 22:36:47 -0400

    helper function complete

--- Medium/singleCycleCheck.java
+++ Medium/singleCycleCheck.java
@@ -1,3 +1,5 @@
+import jdk.internal.org.jline.terminal.impl.CursorSupport;
+
 public class singleCycleCheck {
     public static boolean hasSingleCycle(int[] array) {
         int numElementsVisited = 0;
@@ -20,5 +22,17 @@ public class singleCycleCheck {
     }
 
     // create helper function
-    
+    public static getNextIdx(int currentIdx, int[] array) {
+        // create reference point for element at the index of (currentIdx)
+        int jump = array[currentIdx];
+
+        // create a variable to store the remainder value of the 
+        // sum of (currentIdx) and (jump) modulo the length of 
+        // the array
+        int next = (currentIdx + jump) % array.length;
+
+        // ternary operator to check if the value of (next) is greater than
+        // or equal to 0
+        return next >= 0 ? next : next + array.length;
+    }
 }
