c8afda92131007ab7d5ca41d6b93c7e9cd921ed7
pairofAces <karanchauhan16@gmail.com>
2021-10-01 22:26:06 -0400

    generated templates for class methods required for BST construction

--- Medium/bstConstruction.java
+++ Medium/bstConstruction.java
@@ -8,4 +8,24 @@ class Program {
             this.value = value;
         }
     }
+
+    // create BST insert method
+    public BST insert (int value) {
+
+    }
+
+    // create a boolean contains method
+    public boolean contains(int value) {
+
+    }
+
+    // create BST remove method
+    public BST remove(int value, BST parent) {
+
+    }
+
+    // create a method to get the min value
+    public int getMinVal() {
+
+    }
 }
