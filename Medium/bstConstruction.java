e55f480ceffbb4c4696e662dc108708f5c1a78ca
pairofAces <karanchauhan16@gmail.com>
2021-10-01 22:20:47 -0400

    creating program to construct Binary Search Tree

---
+++ Medium/bstConstruction.java
@@ -0,0 +1,11 @@
+class Program {
+    public static BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST (int value) {
+            this.value = value;
+        }
+    }
+}
