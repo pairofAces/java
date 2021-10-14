b80f7e9f67d6372c42cc634594545632ecd1acd5
Karan Chauhan <karanchauhan@Karans-Air.cable.rcn.com>
2021-10-13 22:12:26 -0400

    creating logic

--- Easy/tournamentWinner.java
+++ Easy/tournamentWinner.java
@@ -1,3 +1,16 @@
+import java.util.*;
+
 class Program {
-    
+    public int HOME_TEAM_WON = 1;
+
+    public String tournamentWinner(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
+        String currentBest = "";
+        // logic
+
+        // create hashmap to store scores
+        HashMap<String, Integer> scores = new HashMap<String, Integer>();
+        scores.put(currentBest, 0);
+        
+        return currentBest;
+    }
 }
