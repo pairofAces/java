02800f649771a8796b20ae203342627353371090
pairofAces <karanchauhan16@gmail.com>
2021-10-19 23:38:20 -0400

    created solution for non-constructible change

--- Easy/nonConstructibleChange.java
+++ Easy/nonConstructibleChange.java
@@ -2,6 +2,26 @@ import java.util.*;
 
 public class nonConstructibleChange {
     public int nonConstructibleChangeMain(int[] coins) {
-        // create logic hear
+        // sort the coins array
+        Arrays.sort(coins);
+
+        // create variable to reference the current amount of change.
+        int currentChange = 0;
+
+        // traverse through the sorted coins array
+        for (int coin : coins) {
+            // if the value of the current coin is greater than the value of (currentChange + 1)
+            if (coin > currentChange + 1) {
+                // return the value of (currentChange + 1)
+                return currentChange + 1;
+            }
+
+            // increment the value of (currentChange) by the value of the current coin
+            currentChange += coin;
+        }
+
+        // after the for loop, the value of (currentChange + 1) will be the lowest amount of change
+        // that cannot be created
+        return currentChange + 1;
     }
 }
