a926ea0a465f5df8b7b093b64f493b78ed8f0ea4
pairofAces <karanchauhan16@gmail.com>
2021-10-21 23:20:06 -0400

    working on for loop logic

--- Medium/threeNumberSum.java
+++ Medium/threeNumberSum.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 public class threeNumberSum {
     public static List<Integer[]> threeNumSum(int[] array, int targetSum) {
-        
+        // sort the input array
+        Arrays.sort(array);
+        List<Integer []> triplets = new ArrayList<Integer[]>();
+
+        // traverse through the sorted array
+        for (int i = 0; i < array.length - 2; i++) {
+            
+        }
     }
 }
